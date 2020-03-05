3a08152a785c1fefbb0c6cea65fe7c67e81f06a8
Maria Clara Santana <claarasantana@gmail.com>
2020-03-05 12:12:20 -0300

    feat: create helper to group reviews by users

---
+++ src/utils/group-reviews-by-user.ts
@@ -0,0 +1,23 @@
+import gamesJson from "../data/games.json";
+import usersJson from "../data/users.json";
+import {
+  UserInterface,
+  GameInterface,
+  ReviewsByUserInterface,
+  UserReviewInterface
+} from "../types.js";
+
+const users: UserInterface[] = usersJson.data;
+const games: GameInterface[] = gamesJson.data;
+
+export default (): ReviewsByUserInterface[] =>
+  users.map(user => {
+    const userId = user.id;
+    const reviews = games.reduce((acc, game) => {
+      const review = game.reviews.find(r => r.userId === userId);
+      if (!review) return acc;
+      return [...acc, { movieId: game.id, rating: review?.rating }];
+    }, [] as UserReviewInterface[]);
+
+    return { userId, reviews };
+  });
