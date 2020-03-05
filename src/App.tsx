6bb9c4ad45692be35c41dfbe2d37c8fa7ebb9e2e
Maria Clara Santana <claarasantana@gmail.com>
2020-03-05 11:39:13 -0300

    feat: select random user and add recommended to select

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback, useState, ChangeEvent } from "react";
+import React, { useCallback, useState, ChangeEvent, useEffect } from "react";
 import Game from "./components/Game";
-import { GameInterface, Options } from "./types";
+import { GameInterface, Options, UserInterface } from "./types";
 import jsonGames from "./data/games.json";
+import jsonUsers from "./data/users.json";
+import randomIntFromInterval from "./utils/random-int-from-interval";
 import "./App.css";
 
 const selectOptions = [
@@ -16,6 +18,10 @@ const selectOptions = [
   {
     label: "Rating",
     value: Options.RATING
+  },
+  {
+    label: "Recommended",
+    value: Options.RECOMMENDED
   }
 ];
 
@@ -26,12 +32,19 @@ const sortHelper = (games: GameInterface[], sortType: Options) => {
 };
 
 const App = () => {
+  const [user, setUser] = useState<UserInterface>();
   const [games, setGames] = useState<GameInterface[]>(jsonGames.data);
 
-  const updateGames = useCallback(
+  useEffect(() => {
+    const userDB = jsonUsers.data;
+    const randomUser = userDB[randomIntFromInterval(0, userDB.length)];
+    setUser(randomUser);
+  }, []);
+
+  const sortGames = useCallback(
     (e: ChangeEvent<HTMLSelectElement>) => {
-      const gamesData = games.slice(0);
       const selectedOption = e.target.value as Options;
+      const gamesData = games.slice(0);
       const sortedGames =
         selectedOption === Options.POPULARITY
           ? jsonGames.data
@@ -43,23 +56,28 @@ const App = () => {
 
   return (
     <div className="vertical app">
-      <div className="select">
-        <label htmlFor="change-order">Order by:&nbsp;</label>
-        <select id="change-order" onChange={updateGames}>
-          {selectOptions.map((option, index) => (
-            <option key={index.toString()} value={option.value}>
-              {option.label}
-            </option>
+      <header className="horizontal app-header">
+        <h2>{`Hello, ${user?.name || "gamer"}`}</h2>
+        <div className="select">
+          <label htmlFor="change-order">Order by:&nbsp;</label>
+          <select id="change-order" onChange={sortGames}>
+            {selectOptions.map((option, index) => (
+              <option key={index.toString()} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </div>
+      </header>
+      <main>
+        <ul className="games">
+          {games.map(game => (
+            <li key={game.id}>
+              <Game game={game} />
+            </li>
           ))}
-        </select>
-      </div>
-      <ul className="games">
-        {games.map(game => (
-          <li key={game.id}>
-            <Game game={game} />
-          </li>
-        ))}
-      </ul>
+        </ul>
+      </main>
     </div>
   );
 };
