a5c60b521551cf9731c1215715cae7f2b44f7344
Maria Clara Santana <claarasantana@gmail.com>
2020-03-06 13:42:14 -0300

    chore: create-graph util for sort helper

--- src/utils/create-graph.ts
+++ src/utils/create-graph.ts
@@ -1,15 +1,15 @@
 import gamesJson from "../data/games.json";
 import usersJson from "../data/users.json";
-// // import Graph from "../lib/graph/Graph";
-import GraphVertex from "../lib/GraphVertex";
+import Graph from "../lib/graph/Graph";
+import GraphVertex from "../lib/graph/GraphVertex";
 import { UserInterface, GameInterface } from "../types.js";
 
 const users: UserInterface[] = usersJson.data;
 const games: GameInterface[] = gamesJson.data;
 
 export default () => {
-  //   const graph = new Graph();
-  const userVertices = users.map(user => new GraphVertex(user.id));
-  const gamesVertices = games.map(game => new GraphVertex(game.id));
-  return { userVertices, gamesVertices };
+  const graph = new Graph();
+  const vertices = [...users, ...games].map(item => new GraphVertex(item.id));
+  vertices.forEach(v => graph.addVertex(v));
+  return graph;
 };
